8f42fd2f348b9d459770b11b2f23de8a7426de14
murpin <qw04011@gmail.com>
2024-05-14 12:06:05 +0900

    feat: Main Section 작성

--- src/components/Section/Main/index.tsx
+++ src/components/Section/Main/index.tsx
@@ -1,7 +1,20 @@
+import { ReactComponent as BlueSeed } from './Asset/blue_seed.svg';
+import styles from './Main.module.scss';
+
 export default function MainSection() {
   return (
-    <div>
-      Header
+    <div className={styles.section}>
+      <div className={styles.description}>
+        <div className={styles.description__title}>
+          씨앗에서부터 더 높은 단계로<br />
+          나아가기 위한 출발.
+        </div>
+        <div className={styles.description__content}>
+          발아는 단순한 생명의 시작을 넘어, 가능성과 희망,<br />
+          그리고 무한한 잠재력의 상징입니다.
+        </div>
+      </div>
+      <BlueSeed className={styles.section__image} />
     </div>
   )
 }
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,12 @@
+import MainSection from 'components/Section/Main';
+import LocationSection from 'components/Section/Location';
+import styles from './main.module.scss';
+
 export default function Main() {
   return (
-    <div>
-      MAIN
+    <div className={styles.template}>
+      <MainSection />
+      <LocationSection />
     </div>
   )
 }
