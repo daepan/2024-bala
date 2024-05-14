739659f7808bab30da172dbbf67a6eddf7187537
murpin <qw04011@gmail.com>
2024-05-14 14:43:52 +0900

    feat: Header 내용 수정 및 bold 처리

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,32 @@
 import { ReactComponent as Logo } from 'assets/bala_logo.svg';
 import cn from 'utils/ts/classnames';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 export default function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <div className={styles.header}>
-      <Logo onClick={() => navigate('/')}/>
+      <Logo className={styles.header__logo} onClick={() => navigate('/')}/>
       <div className={styles.header__list}>
         <Link className={cn({
           [styles.header__item]: true,
-          [styles['header__item--start']]: true
+          [styles['header__item--start']]: true,
+          [styles['header__item--bold']]: location.pathname === '/about',
         })}
         to="/about">About</Link>
         <Link className={cn({
           [styles.header__item]: true,
-          [styles['header__item--bar']]: true
+          [styles['header__item--bar']]: true,
+          [styles['header__item--bold']]: location.pathname === '/project',
         })}
         to="/project">Project</Link>
         <Link className={cn({
           [styles.header__item]: true,
-          [styles['header__item--lastbar']]: true
+          [styles['header__item--lastbar']]: true,
+          [styles['header__item--bold']]: location.pathname === '/designer',
         })}
         to="/designers">Designers</Link>
         <div className={cn({
