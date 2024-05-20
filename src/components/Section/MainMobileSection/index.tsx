a1fb45e374ae29d0088157852e42dd406bf96e00
murpin <qw04011@gmail.com>
2024-05-20 15:29:11 +0900

    feat: 모바일 메인페이지 대응

--- src/components/Section/MainMobileSection/index.tsx
+++ src/components/Section/MainMobileSection/index.tsx
@@ -1,7 +1,50 @@
-export default function MainMobileSection() {
+import React from 'react';
+import { ReactComponent as MainLogo } from 'assets/bala_logo.svg';
+import { ReactComponent as Arrow } from 'components/Section/Main/Asset/arrow.svg';
+import { ReactComponent as BlueSeed } from 'components/Section/Main/Asset/blue_seed.svg';
+import {
+  useScroll,
+  useTransform,
+  MotionValue,
+  motion,
+} from "framer-motion";
+import styles from './MainMobileSection.module.scss';
+
+interface MainMobileSectionProps {
+  onMoveScroll: () => void
+}
+
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
+export default function MainMobileSection({
+  onMoveScroll
+}: MainMobileSectionProps) {
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 300); 
+
   return (
-    <div>
-      MAINMOBILE
+    <div className={styles.section}>
+      <motion.div className={styles.section__image} style={{ y }}>
+        <BlueSeed width={546.389} height={546.389} />
+      </motion.div>
+      <div className={styles.section__describe}>
+        <div className={styles.header}>
+          <MainLogo width={174.8} height={24.81} />
+          <div className={styles.header__title}>
+          한국기술교육대학교 디자인공학과 30th 졸업전시
+          </div>
+        </div>
+        <div className={styles.content}>
+        발아는 단순한 생명의 시작을 넘어, <br />
+        가능성과 희망, 그리고 무한한 잠재력의 상징입니다. <br />
+        더 멀리 낯선 곳에서 꽃 피우기 위한 우리의 도약을 <br />
+        이번 전시를 통해 선보이고자 합니다. <br />
+        </div>
+        <Arrow onClick={() => onMoveScroll()} />
+      </div>
     </div>
   )
 }
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,22 @@
 import useMoveScroll from 'utils/hooks/useMoveToScroll';
+import useMediaQuery from 'utils/hooks/useMediaQuery';
 import MainSection from 'components/Section/Main';
+import MainMobileSection from 'components/Section/MainMobileSection';
 import LocationSection from 'components/Section/Location';
 import styles from './main.module.scss';
 
 export default function Main() {
   const { scrollTo, onMoveToElement } = useMoveScroll();
+  const isMobile = useMediaQuery();
   return (
     <div className={styles.template}>
-      <MainSection onMoveScroll={() => onMoveToElement()} />
+      {
+        isMobile ? (
+          <MainMobileSection onMoveScroll={() => onMoveToElement()} />
+        ) : (
+          <MainSection onMoveScroll={() => onMoveToElement()} />
+        )
+      }
       <div ref={scrollTo} />
       <LocationSection />
     </div>
