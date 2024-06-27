7121b7d0064db0ca00eab84a57da7c52e9f63347
daepan <qw04011@gmail.com>
2024-06-27 23:23:43 +0900

    [DATA] 이미지 내용 및 데이터 변경

---
+++ src/utils/constant/projects.ts
@@ -0,0 +1,377 @@
+export const PROJECTS = [
+  {
+    name: '원윤섭',
+    productName: 'Soffi',
+    category: 'Mobility',
+    desginers: [
+      {
+        student_number: 2018151023,
+        name: '원윤섭',
+        name_en: 'Yoon-Seop Won'
+      }
+    ],
+    title: '헬멧을 쾌적하게 사용하도록 해주는 테이블 오브제'
+  },
+  {
+    name: '김혜민, 이수빈',
+    productName: 'Climb',
+    category: 'Mobility',
+    desginers: [
+      {
+        student_number: 2021151018,
+        name: '김혜민',
+        name_en: 'Hye-Min Kim'
+      },
+      {
+        student_number: 2021151052,
+        name: '이수빈',
+        name_en: 'Soo-Bin Lee'
+      }
+    ],
+    title: '계단 청소를 도와주는 스텝 클라이머'
+  },
+  {
+    name: '조용현',
+    productName: 'Stretchroller',
+    category: 'Mobility',
+    desginers: [
+      {
+        student_number: 2019151036,
+        name: '조용현',
+        name_en: 'Yong-Hyeon Cho'
+      }
+    ],
+    title: '차량탑승의 편의성을 고려한 대형견 유모차'
+  },
+  {
+    name: '정해민, 사공도영',
+    productName: 'GlideSurf',
+    category: 'Mobility',
+    desginers: [
+      {
+        student_number: 2019151035,
+        name: '정해민',
+        name_en: 'Hae-Min Jung'
+      },
+      {
+        student_number: 2020151023,
+        name: '사공도영',
+        name_en: 'Do-Young Sakong'
+      }
+    ],
+    title: '수상 친화적 레저 스포츠 모빌리티'
+  },
+  {
+    name: '강호정, 김다준',
+    productName: 'Seader',
+    category: 'Mobility',
+    desginers: [
+      {
+        student_number: 2021151002,
+        name: '강호정',
+        name_en: 'Ho-Jeong Kang'
+      },
+      {
+        student_number: 2019151003,
+        name: '김다준',
+        name_en: 'Da-Joon Kim'
+      }
+    ],
+    title: '바다숲 복원을 돕는 잘피 이식 모빌리티'
+  },
+  {
+    name: '김지민, 백승선',
+    productName: 'illo',
+    category: 'Mobility',
+    desginers: [
+      {
+        student_number: 2021151014,
+        name: '김지민',
+        name_en: 'Ji-Min Kim'
+      },
+      {
+        student_number: 2019151020,
+        name: '백승선',
+        name_en: 'Seung-Seon Baek'
+      }
+    ],
+    title: '전기차 충전 인프라의 효율을 높이기 위한 배터리 딜리버리 로봇'
+  },
+  {
+    name: '신민우, 이승은',
+    productName: 'Heasy',
+    category: 'Mobility',
+    desginers: [
+      {
+        student_number: 2019151023,
+        name: '신민우',
+        name_en: 'Min-Woo Shin'
+      },
+      {
+        student_number: 2021151037,
+        name: '이승은',
+        name_en: 'Seung-Eun Lee'
+      }
+    ],
+    title: '대중교통 연계형 접이식 전동 PM'
+  },
+  {
+    name: '곽우령',
+    productName: 'S-C-Roll',
+    category: 'Mobility',
+    desginers: [
+      {
+        student_number: 2021151005,
+        name: '곽우령',
+        name_en: 'Woo-Ryoung Kwak'
+      }
+    ],
+    title: '2차 사고 방지를 위한 차량용 안전삼각대'
+  },
+  {
+    name: '김승규, 김익현',
+    productName: 'EZ-Beesy',
+    category: 'Care',
+    desginers: [
+      {
+        student_number: 2019151008,
+        name: '김승규',
+        name_en: 'Seung-Gyu Kim'
+      },
+      {
+        student_number: 2019151010,
+        name: '김익현',
+        name_en: 'Ik-Hyeon Kim'
+      }
+    ],
+    title: '소방관을 위한 휴대용 벌 포집기'
+  },
+  {
+    name: '이예빈',
+    productName: 'Fegeres',
+    category: 'Care',
+    desginers: [
+      {
+        student_number: 2021151038,
+        name: '이예빈',
+        name_en: 'Ye-Bin Lee'
+      }
+    ],
+    title:
+      '화재 진압과정에서 사용자의 화상을 방지할 수 있는 공공장소형 방패진압 소화기'
+  },
+  {
+    name: '오기석',
+    productName: 'Roof',
+    category: 'Care',
+    desginers: [
+      {
+        student_number: 2021151051,
+        name: '오기석',
+        name_en: 'Ki-Seok Oh'
+      }
+    ],
+    title: '구조 대원 매몰 사고를 대비한 웨어러블 장비'
+  },
+  {
+    name: '김두언, 최성일',
+    productName: 'FORTO',
+    category: 'Care',
+    desginers: [
+      {
+        student_number: 2019151005,
+        name: '김두언',
+        name_en: 'Du-Eon Kim'
+      },
+      {
+        student_number: 2019151039,
+        name: '최성일',
+        name_en: 'Seong-Il Choe'
+      }
+    ],
+    title: '업사이클링을 통한 영유아 케어용 조립식 가구'
+  },
+  {
+    name: '박채연, 최혁수',
+    productName: 'Savuoy',
+    category: 'Care',
+    desginers: [
+      {
+        student_number: 2021151024,
+        name: '박채연',
+        name_en: 'Chae-Yeon Park'
+      },
+      {
+        student_number: 2019151040,
+        name: '최혁수',
+        name_en: 'Hyeok-Su Choi'
+      }
+    ],
+    title: '이안류에 대비한 구조용 부표'
+  },
+  {
+    name: '황민성, 천세강',
+    productName: 'JOYNT',
+    category: 'Care',
+    desginers: [
+      {
+        student_number: 2019151045,
+        name: '황민성',
+        name_en: 'Min-Seong Hwang'
+      },
+      {
+        student_number: 2019151038,
+        name: '천세강',
+        name_en: 'Se-Kang Chun'
+      }
+    ],
+    title: '휠체어 탈착이 가능한 관절구축 예방 재활기기'
+  },
+  {
+    name: '이시온, 최민경',
+    productName: 'Walkmate',
+    category: 'Care',
+    desginers: [
+      {
+        student_number: 2020151037,
+        name: '이시온',
+        name_en: 'Si-On Lee'
+      },
+      {
+        student_number: 2020151045,
+        name: '최민경',
+        name_en: 'Min-Kyeong Choe'
+      }
+    ],
+    title: '보행 시 낙상의 위험을 최소화 하는 보행보조차'
+  },
+  {
+    name: '김이연, 김수현',
+    productName: '낑',
+    category: 'Living',
+    desginers: [
+      {
+        student_number: 2021151011,
+        name: '김이연',
+        name_en: 'I-Youn Kim'
+      },
+      {
+        student_number: 2019151007,
+        name: '김수현',
+        name_en: 'Su-Hyeon Kim'
+      }
+    ],
+    title: '이끼를 활용한 친환경 실내용 공기정화가습기'
+  },
+  {
+    name: '신동찬, 최성우',
+    productName: 'futri',
+    category: 'Living',
+    desginers: [
+      {
+        student_number: 2021151028,
+        name: '신동찬',
+        name_en: 'Dong-Chan Shin'
+      },
+      {
+        student_number: 2021151043,
+        name: '최성우',
+        name_en: 'Sung-Woo Choi'
+      }
+    ],
+    title: '패치 생성 기술을 이용한 커스텀 패치 생성기, 웨어러블 기기'
+  },
+  {
+    name: '김도훈, 김호빈',
+    productName: 'PLAYBOOK',
+    category: 'Living',
+    desginers: [
+      {
+        student_number: 2021151049,
+        name: '김도훈',
+        name_en: 'Do-Hoon Kim'
+      },
+      {
+        student_number: 2021151050,
+        name: '김호빈',
+        name_en: 'Ho-Been Kim'
+      }
+    ],
+    title: '무선 디스플레이 기술을 활용한 게이밍 폼팩터'
+  },
+  {
+    name: '이한',
+    productName: 'Re:Ver',
+    category: 'Living',
+    desginers: [
+      {
+        student_number: 2019151032,
+        name: '이한',
+        name_en: 'Han Lee'
+      }
+    ],
+    title: '캡슐 분리배출이 간편한 캡슐 커피머신'
+  },
+  {
+    name: '김채은, 박정빈',
+    productName: '북르륵',
+    category: 'Living',
+    desginers: [
+      {
+        student_number: 2021151016,
+        name: '김채은',
+        name_en: 'Chae-Eun Kim'
+      },
+      {
+        student_number: 2020151019,
+        name: '박정빈',
+        name_en: 'Jung-Bin Park'
+      }
+    ],
+    title: '도서관 사서의 편안한 배가 업무를 위한 북카트 디자인'
+  },
+  {
+    name: '이서하',
+    productName: 'Hiking',
+    category: 'Living',
+    desginers: [
+      {
+        student_number: 2019151033,
+        name: '이서하',
+        name_en: 'Seo-Ha Lee'
+      }
+    ],
+    title: '허리건강을 위한 높이조절 전동 쇼핑카트'
+  },
+  {
+    name: '박지윤, 윤채원',
+    productName: '맡겨줄개',
+    category: 'Living',
+    desginers: [
+      {
+        student_number: 2021151023,
+        name: '박지윤',
+        name_en: 'Ji-Yun Park'
+      },
+      {
+        student_number: 2020151032,
+        name: '윤채원',
+        name_en: 'Chae-Won Yun'
+      }
+    ],
+    title: '편의점 출입 시 이용하는 반려견 맡김 서비스'
+  },
+  {
+    name: '김정연',
+    productName: 'Aira',
+    category: 'Living',
+    desginers: [
+      {
+        student_number: 2021151012,
+        name: '김정연',
+        name_en: 'Jeong-Yeon Kim'
+      }
+    ],
+    title: '보행이 불편한 반려견을 위한 휠체어'
+  }
+];
