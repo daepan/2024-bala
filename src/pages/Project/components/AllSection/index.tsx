aa2cb871ee587e1189a6ddd8296f17d467405c3b
daepan <qw04011@gmail.com>
2024-07-03 01:54:48 +0900

    feat: 프로젝트 셔플 기능 추가

--- src/pages/Project/components/AllSection/index.tsx
+++ src/pages/Project/components/AllSection/index.tsx
@@ -2,7 +2,10 @@ import DetailProjectItem from '../DetailProjectItem';
 import styles from './AllSection.module.scss';
 import { PROJECTS, ProjectDataType } from 'utils/constant/projects';
 
-export function shuffleProjectArray(array: ProjectDataType[]): ProjectDataType[] {
+export function shuffleProjectArray(
+  array: ProjectDataType[] | undefined
+): ProjectDataType[] {
+  if (array === undefined) return [];
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -10,21 +13,20 @@ export function shuffleProjectArray(array: ProjectDataType[]): ProjectDataType[]
   return array;
 }
 
-
 function AllSection() {
-  
   return (
     <div className={styles['container']}>
       <div className={styles['item-section']}>
-        {shuffleProjectArray(PROJECTS).map((project) => (
-          <DetailProjectItem 
-          key={project.name}
-          title={project.title}
-          name={project.name} 
-          productName={project.productName} 
-          category={project.category}
-          imgType={project.imgType}
-          />))}
+        {shuffleProjectArray(PROJECTS).map(project => (
+          <DetailProjectItem
+            key={project.name}
+            title={project.title}
+            name={project.name}
+            productName={project.productName}
+            category={project.category}
+            imgType={project.imgType}
+          />
+        ))}
       </div>
     </div>
   );
